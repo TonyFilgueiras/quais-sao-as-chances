9d28a332b604846a757076c9b2c42bce2578c447
Tony Filgueiras <ant.mosquera@yahoo.com.br>
2023-11-28 02:05:14 -0300

    adding header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
+  <HeaderBar />
   <HomeView />
 </template>
 
-  <script lang="ts">
-  // import { RouterLink, RouterView } from 'vue-router'
-  import  HomeView from './views/HomeView.vue';
+<script lang="ts">
+// import { RouterLink, RouterView } from 'vue-router'
+import HomeView from "./views/HomeView.vue";
+import HeaderBar from "./components/HeaderBar.vue";
 
-  export default {
-    components: {
-      HomeView,
-    },
-  }
-  </script>
+export default {
+  components: {
+    HomeView,
+    HeaderBar,
+  },
+};
+</script>
 
-<style >
-:root{
-    --champion: #00ff0088;
-    --libertadores: #4444ff88;
-    --preLibertadores: #44ffff88;
-    --sulAmericana: #ffff0088;
-    --neutro: #eeeeee88;
-    --rebaixamento: #ff000088;
-    --brasileiraoBlue: #004498;
-    --brasileiraoGold: #bfa706;
-    --brasileiraoSilver: #a5a4a9;
+<style>
+:root {
+  --champion: #00ff0088;
+  --libertadores: #4444ff88;
+  --preLibertadores: #44ffff88;
+  --sulAmericana: #ffff0088;
+  --neutro: #eeeeee88;
+  --rebaixamento: #ff000088;
+  --brasileiraoBlue: #004498;
+  --brasileiraoGold: #bfa706;
+  --brasileiraoSilver: #a5a4a9;
 }
 </style>
---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,69 @@
+<template>
+  <header>
+    <h1>Quais são as chances</h1>
+    <!-- <MenuIcon color="white" width="30"/> -->
+    <img src="@/assets/brasileirao-logo.png" alt="Brasileirao" title="Brasileirao" />
+    <AboutIcon width="30" @click="toggleAboutModel" />
+    <AboutModel :show-about-model="showAboutModel" @close="closeAboutModel"/>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import AboutIcon from "./AboutIcon.vue";
+import AboutModel from "./AboutModel.vue";
+// import MenuIcon from './MenuIcon.vue';
+
+export default defineComponent({
+  data() {
+    return {
+      showAboutModel: false,
+    };
+  },
+  methods: {
+    toggleAboutModel() {
+      this.showAboutModel = !this.showAboutModel;
+    },
+    closeAboutModel() {
+      this.showAboutModel = false;
+    },
+    handleDocumentClick(event: MouseEvent) {
+      if (this.showAboutModel && !this.$el.contains(event.target)) {
+        this.closeAboutModel();
+      }
+    },
+  },
+  mounted() {
+    // Add a click event listener on the entire document
+    document.addEventListener("click", this.handleDocumentClick);
+  },
+  beforeUnmount() {
+    // Remove the click event listener when the component is destroyed
+    document.removeEventListener("click", this.handleDocumentClick);
+  },
+  components: { AboutIcon, AboutModel },
+});
+</script>
+
+<style scoped>
+header {
+  background: var(--brasileiraoBlue);
+  color: white;
+  padding: 10px 20px;
+  height: 60px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+h1 {
+  font-size: 1.5rem;
+  font-family: "Playfair Display", Arial, Helvetica, sans-serif;
+  font-weight: bold;
+}
+img {
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  height: 50px;
+}
+</style>
