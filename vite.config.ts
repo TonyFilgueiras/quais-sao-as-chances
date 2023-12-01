350e621031e3084ec09f85cb1e9e146a1c84e435
Tony Filgueiras <ant.mosquera@yahoo.com.br>
2023-11-30 22:19:11 -0300

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "quais-sao-as-chances",
+  base: "/quais-sao-as-chances/",
   plugins: [
     vue(),
   ],
