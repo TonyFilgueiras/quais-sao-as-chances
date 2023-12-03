261f33837cfb9c8326afc2dac8e2a6cf9550cac2
Tony Filgueiras <ant.mosquera@yahoo.com.br>
2023-12-03 07:37:29 -0300

    web history

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.VITE_APP_BASE_URL),
+  history: createWebHistory("http://127.0.0.1:5000"),
   routes: [
     {
       path: '/',
