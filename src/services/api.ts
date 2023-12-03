1ed1555b32bf76ce4d65dc63d4812aa7885d7f04
Tony Filgueiras <ant.mosquera@yahoo.com.br>
2023-12-03 07:26:30 -0300

    removing api key

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,17 +6,9 @@ const http = axios.create({
 
 export default {
     getChampionshipTable() {
-        return http.get(`/api/standings`, {
-            headers: { 
-                Authorization: `Bearer ${import.meta.env.VITE_APP_TEST_API_KEY}`
-            }
-        })
+        return http.get(`/api/standings`)
     },
     getChampionshipFixtures() {
-        return http.get(`/api/fixtures`, {
-            headers: { 
-                Authorization: `Bearer ${import.meta.env.VITE_APP_TEST_API_KEY}`
-            }
-        })
+        return http.get(`/api/fixtures`)
     },
 }
