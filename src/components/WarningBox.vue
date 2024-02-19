1dfa77b3d24e0c50b1d8453660943d4aab669450
TonyFilgueiras <ant.mosquera@yahoo.com.br>
2024-02-19 11:23:44 -0300

    feat: adding warningbox component

---
+++ src/components/WarningBox.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>{{warningText}}</h2>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  props: {
+    warningText: {
+      type: String, 
+    },
+  },
+}
+</script>
+<style scoped>
+  div{
+    background-color: var(--sulAmericana);
+  }
+</style>
