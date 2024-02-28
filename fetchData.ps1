496c109c70e5338c18619af5e9d2fd0e6bdc52d0
TonyFilgueiras <ant.mosquera@yahoo.com.br>
2024-02-28 18:52:13 +0000

    data update

---
+++ fetchData.ps1
@@ -0,0 +1,15 @@
+cd C:\Users\Administrator\Desktop\quais-sao-as-chances
+
+git pull
+
+cd backend
+
+# Run your Python script
+python get_info.py
+
+cd ../
+
+# Git commands
+git add .
+git commit -m "data update"
+git push
